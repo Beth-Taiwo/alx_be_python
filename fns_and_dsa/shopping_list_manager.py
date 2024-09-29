30ecc2369053dfd266c39879e530f718d15f3ede
Beth Taiwo <popoolataiwo22@gmail.com>
2024-09-29 19:18:46 +0000

    added space to input prompt

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -17,7 +17,7 @@ def main():
             shopping_list.append(item)
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("Enter the item you want to remove")
+            item = input("Enter the item you want to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
             else:
