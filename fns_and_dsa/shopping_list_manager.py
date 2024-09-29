681717c93be9a9f3897e9843012e5e48cc2eb6a6
Beth Taiwo <popoolataiwo22@gmail.com>
2024-09-29 19:27:36 +0000

    check data type

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -9,29 +9,32 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = int(input("Enter your choice: "))
-
-        if choice == '1':
-            # Prompt for and add an item
-            item = input("Enter the item you want to add: ")
-            shopping_list.append(item)
-        elif choice == '2':
-            # Prompt for and remove an item
-            item = input("Enter the item you want to remove: ")
-            if item in shopping_list:
-                shopping_list.remove(item)
+        choice = input("Enter your choice: ")
+        if choice.isnumeric(): 
+            if choice == '1':
+                # Prompt for and add an item
+                item = input("Enter the item you want to add: ")
+                if item.isalpha():
+                    shopping_list.append(item)
+            elif choice == '2':
+                # Prompt for and remove an item
+                item = input("Enter the item you want to remove: ")
+                if item in shopping_list:
+                    shopping_list.remove(item)
+                else:
+                    print("Item not found in the list.")
+            elif choice == '3':
+                # Display the shopping list
+                print("Shopping List:")
+                for item in shopping_list:
+                    print(item)
+            elif choice == '4':
+                print("Goodbye!")
+                break
             else:
-                print("Item not found in the list.")
-        elif choice == '3':
-            # Display the shopping list
-            print("Shopping List:")
-            for item in shopping_list:
-                print(item)
-        elif choice == '4':
-            print("Goodbye!")
-            break
+                print("Invalid choice. Please try again.")
         else:
-            print("Invalid choice. Please try again.")
+            print("Invalid choice. Please enter a number.")
 
 if __name__ == "__main__":
     main()
