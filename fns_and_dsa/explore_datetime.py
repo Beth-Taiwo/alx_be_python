a642353e31f77a19edd57be97c031ee19a530990
Beth Taiwo <popoolataiwo22@gmail.com>
2024-09-29 20:02:33 +0000

    Exploring  Module

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,12 @@
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    print(f"Current date and time: {current_date.strftime('%Y-%m-%d %H:%M:%S')}")
+
+no_of_days = int(input('Enter the number of days to add to the current date: '))
+
+def calculate_future_date(no_of_days):
+    future_date = datetime.datetime.now() + datetime.timedelta(days=no_of_days)
+    print(f"Future date: {future_date.strftime('%Y-%m-%d')}")
+  
