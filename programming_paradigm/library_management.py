c66ee5ee40f9fb172af003db42b5d4ee12f307f2
Beth Taiwo <popoolataiwo22@gmail.com>
2024-10-06 23:09:45 +0000

    added methods to Book class

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -3,6 +3,12 @@ class Book:
         self.title = title
         self.author = author
         self._is_checked_out = False  # Private attribute
+        
+    def check_out_book(self):
+        self._is_checked_out = True
+        
+    def return_book(self):
+        self._is_checked_out = False  # Set book to available again
     
 class Library:
     def __init__(self):
@@ -14,16 +20,12 @@ class Library:
     def check_out_book(self, title):
         for book in self._books:
             if book.title == title and not book._is_checked_out:
-                book._is_checked_out = True
-                return True
-        return False
+                book.check_out_book()
     
     def return_book(self, title):
         for book in self._books:
             if book.title == title and book._is_checked_out:
-                book._is_checked_out = False
-                return True
-        return False
+                book.return_book()
     
     def list_available_books(self):
         available_books = [f"{book.title} by {book.author}" for book in self._books if not book._is_checked_out]
