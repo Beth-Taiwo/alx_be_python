38bf01927edf7f869529ab22182f572c380a5d70
Beth Taiwo <popoolataiwo22@gmail.com>
2024-10-13 18:01:35 +0000

    passed dunder method

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,16 +2,25 @@ class Book:
     def __init__(self,title,author):
         self.title = title
         self.author = author
+        
+    def __str__(self):
+        return f"{self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self,title,author,file_size):
         super().__init__(title, author)
         self.file_size = file_size
+        
+    def __str__(self):
+        return f"{self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self,title,author,page_count):
         super().__init__(title, author)
         self.page_count = page_count
+        
+    def __str__(self):
+        return f"{self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
@@ -22,10 +31,5 @@ class Library:
         
     def list_books(self):
         for book in self.books:
-            if book.__class__.__name__ == 'Book':
-                print(f"{book.__class__.__name__}: {book.title} by {book.author}")
-            elif book.__class__.__name__ == 'EBook':
-                print(f"{book.__class__.__name__}: {book.title} by {book.author}, File Size: {book.file_size}KB")
-            elif book.__class__.__name__ == 'PrintBook':
-                print(f"{book.__class__.__name__}: {book.title} by {book.author}, Page Count: {book.page_count}")
+            print(f"{book.__class__.__name__}: {book}")
 
